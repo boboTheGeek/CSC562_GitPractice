b22475fcb86a400636ba02657403f0234aea39bf
Rob Miles <rob.miles@volvo.com>
2018-05-30 11:16:45 -0400

    updating dataMethod()

--- Miles.java
+++ Miles.java
@@ -8,9 +8,17 @@ public class Miles
 	{
 	}
 
-	public void dataMethod()
+	public int dataMethod()
 	{
+	    int result = 0;
+	    for (int x=0;x<name.length();x++)
+	    {
+	        result = name.charAt(x) + result;
+	    }
+	    data = result % name.length();
+	    return result;
 	}
+
 
 	@Override
 	public String toString()
