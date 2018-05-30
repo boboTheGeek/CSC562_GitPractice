a1c1d9269acd88948ae052392e588bce18a9f985
Rob Miles <rob.miles@volvo.com>
2018-05-30 11:36:12 -0400

    updatedDataMethod()

--- randomP/Miles.java
+++ randomP/Miles.java
@@ -12,14 +12,11 @@ public class Miles
 
 	public int dataMethod()
 	{
-	    int result = 0;
-	    for (int x=0;x<name.length();x++)
-	    {
-	        result = name.charAt(x) + result;
-	    }
+	    int result = 10;
 	    data = result % name.length();
 	    return result;
 	}
+
 
 
 	@Override
