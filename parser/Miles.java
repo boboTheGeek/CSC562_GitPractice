50d85971bdf44a593c85e1335aca4ba120646701
Unknown <chand.gogineni9@gmail.com>
2018-05-30 11:29:00 -0400

    Moving Miles class into parser package

---
+++ parser/Miles.java
@@ -0,0 +1,32 @@
+package parser;
+
+public class Miles
+{
+	int data;
+	String name;
+
+	public void nameMethod(String value)
+	{
+		name = value.substring(1);
+	}
+
+	public int dataMethod()
+	{
+	    int result = 0;
+	    for (int x=0;x<name.length();x++)
+	    {
+	        result = name.charAt(x) + result;
+	    }
+	    data = result % name.length();
+	    return result;
+	}
+
+
+	@Override
+	public String toString()
+	{
+		String fancyName = "X "+name+" X";
+	    return fancyName+" : "+data;
+	}
+
+}
