bca27b3b16203bc7bd8b7dcca5fa7bae8fa477ee
Rob Miles <rob.miles@volvo.com>
2018-05-30 10:30:13 -0400

    initial class

---
+++ Miles.java
@@ -0,0 +1,21 @@
+
+public class Miles
+{
+	int data;
+	String name;
+
+	public void nameMethod()
+	{
+	}
+
+	public void dataMethod()
+	{
+	}
+
+	@Override
+	public String toString()
+	{
+		return name + data;
+	}
+
+}
